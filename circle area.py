ea9472c353d653be9e12a5d73ada57ec584d63e0
gouthamkesav <92319734+gouthamkesav@users.noreply.github.com>
2021-10-13 13:06:44 +0530

    Create circle area.py

---
+++ circle area.py
@@ -0,0 +1,6 @@
+# Python program which accepts the radius of a circle from the user and computes area.
+#A=pi*r**2 circle 
+from math import pi
+#r=float(input('enter the radius of the circle:'));
+r=1.1
+print ("The area of the circle with radius " + str(r) + " is: " + str(pi * r**2))
